662580f61bdda6be305cc21873cee01294f67634
cdog-shen <shencdog@outlook.com>
2025-05-14 17:00:54 +0800

    test: Performance Test

---
+++ test/performance_test.go
@@ -0,0 +1,55 @@
+package test
+
+import (
+	"testing"
+	"time"
+
+	easysnow "github.com/cdog-shen/easy-snow"
+	"github.com/cdog-shen/easy-snow/internal"
+)
+
+func TestGenerateSnowflakeWithUUIDPerformance(t *testing.T) {
+	nodeIDStr := "6ba7b810-9dad-11d1-80b4-00c04fd430c8"
+	err := internal.SetNodeID(nodeIDStr, true)
+	if err != nil {
+		t.Fatalf("Error setting NodeID: %v", err)
+	}
+
+	const numIDs = 500000
+	startTime := time.Now()
+
+	for range numIDs {
+		_, err := easysnow.GenerateSnowflakeID()
+		if err != nil {
+			t.Fatalf("Error creating Meta: %v", err)
+		}
+	}
+
+	endTime := time.Now()
+	elapsedTime := endTime.Sub(startTime)
+
+	t.Logf("Generated %d IDs in %v", numIDs, elapsedTime)
+}
+
+func TestGenerateSnowflakeIDPerformance(t *testing.T) {
+	nodeIDStr := "6ba7b810-9dad-11d1-1232-00c04fd430c8"
+	err := internal.SetNodeID(nodeIDStr, false)
+	if err != nil {
+		t.Fatalf("Error setting NodeID: %v", err)
+	}
+
+	const numIDs = 500000
+	startTime := time.Now()
+
+	for range numIDs {
+		_, err := easysnow.GenerateSnowflakeID()
+		if err != nil {
+			t.Fatalf("Error creating Meta: %v", err)
+		}
+	}
+
+	endTime := time.Now()
+	elapsedTime := endTime.Sub(startTime)
+
+	t.Logf("Generated %d IDs in %v", numIDs, elapsedTime)
+}
